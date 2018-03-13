6d22f4f4598619d909c691f9d90efc00918cd687
Cristiano Chelotti <crisc98@gmail.com>
2018-03-13 19:11:06 -0400

    Final pass

--- Quadratic_hash_table.h
+++ Quadratic_hash_table.h
@@ -4,11 +4,11 @@
  *  - Select the current calendar term and enter the year
  *  - List students with whom you had discussions and who helped you
  *
- * uWaterloo User ID:  uwuserid @uwaterloo.ca
+ * uWaterloo User ID:  czchelot @uwaterloo.ca
  * Submitted for ECE 250
  * Department of Electrical and Computer Engineering
  * University of Waterloo
- * Calender Term of Submission:  (Winter|Spring|Fall) 201N
+ * Calender Term of Submission:  Winter 2018
  *
  * By submitting this file, I affirm that
  * I am the author of all modifications to
@@ -90,135 +90,152 @@ Quadratic_hash_table<Type>::~Quadratic_hash_table(){
 }
 template <typename Type>
 int Quadratic_hash_table<Type>::hash(Type const &obj) const {
-		static_cast<int>(obj) & mask;
+	//Returns the integer portion of the object mod the array size using bitwise AND with mask
+		return (static_cast<int>(obj) & mask);
 }
 template <typename Type>
 int Quadratic_hash_table<Type>::capacity() const {
+	//Returns the max capacity of the hash table
 		return array_size;
 }
 template <typename Type>
 int Quadratic_hash_table<Type>::size() const {
+	//Returns the number of elements in the hash table
 		return count;
 }
 template <typename Type>
 double Quadratic_hash_table<Type>::load_factor() const {
-		return ((count+numDel) / array_size);
+	//Returns the number of occupied bins plus the number of erased bins over the array size
+		return ((double)(count+numDel) / array_size);
 }
 
 template <typename Type>
 bool Quadratic_hash_table<Type>::empty() const {
-	if(array_size == 0)
+	//Returns false if there are no elements in the table
+	if(count == 0)
 		return true;
 	return false;
 }
 template <typename Type>
 Type Quadratic_hash_table<Type>::bin(int n) const {
+	//Returns the value at a specified bin number
 	if(array_size-1 >= n && occupied[n] == OCCUPIED)
 		return array[n];
 }
 template <typename Type>
 bool Quadratic_hash_table<Type>::member(Type const &obj) const {
-	//set probe to be hash function 1's result
+	//Start probing at the has value of the object
 	int probe = hash(obj);
+	//Set initial offset value to 1
 	int offset = 1;
-	//initiate counter that will keep track of number of iterations through table
+	//Initialize counter for number of iterations
 	int counter = 0;
 
-	//while seeing an occupied bin and counter has not reached its limit
-	while(occupied[probe] == OCCUPIED && counter < array_size)
+	//Continue looping while on an occupied or erased bin, making sure that you have not counted more times than the array size
+	while((occupied[probe] == OCCUPIED || occupied[probe] == ERASED) && counter < array_size)
 	{
-		//if element is what we're looking for
-		if(array[probe] == obj)
+		//If we find the object in at this bin
+		if(occupied[probe] == OCCUPIED && array[probe] == obj)
 		{
-			//it means it exists in the hash table so return true
+			//Return true
 			return true;
-			//break out of while loop
+			//Exit loop
 			break;
 		}
-		//if element is not what we're looking for, keep iterating quadraticaly
-		probe +=(offset*offset);
+		//If we do not find the object yet, keep iterating quadratically
+		probe = probe+offset &mask;
 		offset++;
-		//increase counter by one
 		counter++;
 	}
-	//if object is not in hash table return false
+	//If object not in table return false
 	return false;
 }
 
 template <typename Type>
 void Quadratic_hash_table<Type>::insert(Type const &obj){
-	//if hash table is full throw overflow exception
+	//Throw oveflow if table is full
 	if(count == array_size)
 		throw overflow();
 
-	if(member(obj))
-		return;
+		//If the obj is in the table already, exit as we cannot insert it again
+	 if(member(obj))
+	 	return;
 
-	//set probe to start at the hash value of the object
+	//Start probing at the has value
 	int probe = hash(obj);
 	int offset = 1;
-	//if a collision occurs
+
+	//If we try to insert into an occupied bin
 	while(occupied[probe] == OCCUPIED)
 	{
-		//offset the probe by offset^2
-		probe += (offset*offset);
+		//Iterate quadratically until an empty bin is found
+		probe = probe+offset &mask;
 		offset++;
 	}
-	//store object in hash table
+	//Store object in bin
 	array[probe] = obj;
-	//set state of that bin to occupied
+
+	//If we insert into a bin that was previously erased, decrement the number of deleted bins
+	if(occupied[probe] == ERASED){
+		numDel--;
+	}
+	//Set bin to occupied
 	occupied[probe] = OCCUPIED;
-	//increase count by 1
 	count++;
 	return;
 }
 
 template <typename Type>
 bool Quadratic_hash_table<Type>::erase(Type const &obj){
-	if(count == array_size)
-		throw overflow();
 
 	//set probe to start at the hash value of the object
 	int probe = hash(obj);
 	int offset = 1;
 	int counter = 0;
+
+	if(!member(obj)){
+		return false;
+	}
 	//if a collision occurs
-	while(occupied[probe] == OCCUPIED && counter < array_size)
+	while((occupied[probe] == OCCUPIED || occupied[probe] == ERASED) && counter < array_size)
 	{
-		if(array[probe] == obj)
+		if(occupied[probe] == OCCUPIED && array[probe] == obj)
 		{
 			//set value in bin to 0
 			array[probe] = 0;
 			//set state to deleted
 			occupied[probe] = ERASED;
 			//decrease count by 1
+			numDel++;
 			count--;
 			//it means it exists in the hash table so return true
+			//std::cout <<*this<<std::endl;
 			return true;
 		}
 		//offset the probe by offset^2
-		probe += (offset*offset);
+		probe = probe+offset &mask;
 		offset++;
 	}
-	
+	//std::cout <<*this<<std::endl;
+
 	return false;
 }
 
 template <typename Type>
 void Quadratic_hash_table<Type>::clear() {
-	//traverse through entire hash table
+	//Loop through table
 	for(int i = 0; i < array_size; i++)
 	{
-		//set values to 0
+		//Assign bin vals to 0
 		array[i] = 0;
-		//set states to UNOCCUPIED
+		//Set states to UNOCCUPIED
 		occupied[i] = UNOCCUPIED;
 	}
-	//set count to 0
+	//Reset count and numDel to 0
 	count = 0;
+	numDel = 0;
 	return;
 }
-// Your implementation here
 
 template <typename T>
 std::ostream &operator<<( std::ostream &out, Quadratic_hash_table<T> const &hash ) {
